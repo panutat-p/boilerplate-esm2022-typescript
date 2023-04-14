f59950f6b1b28631cabb6f6ab49142820c21b569
panutat-p <pp.panutat@gmail.com>
2023-04-14 20:13:27 +0700

    style: remove semicolon

--- examples/sleep.ts
+++ examples/sleep.ts
@@ -1,9 +1,9 @@
 async function sleep(ms: number): Promise<void> {
-  return new Promise((r) => setTimeout(r, ms));
+  return new Promise((r) => setTimeout(r, ms))
 }
 
-console.log('start');
-await sleep(2000);
-console.log('done');
+console.log('start')
+await sleep(2000)
+console.log('done')
 
-export {};
+export {}
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
-import { getAnime } from './anime.js';
+import { getAnime } from './anime.js'
 
 getAnime()
   .then((r) => console.log(r))
-  .catch((e) => console.log(`🟥 ${JSON.stringify(e)}`));
+  .catch((e) => console.log(`🟥 ${JSON.stringify(e)}`))
 
 const fruits: Record<string, number> = {
   apple: 12,
   '🍌': 11,
   carrot: 10,
   // dragon: undefined, Type 'undefined' is not assignable
-};
+}
 
-console.log('total key:', Object.keys(fruits).length);
+console.log('total key:', Object.keys(fruits).length)
 
 if (fruits.apple) {
-  console.log('apple exists');
+  console.log('apple exists')
 }
 
 if (fruits['🍌']) {
-  console.log('🍌 exists');
+  console.log('🍌 exists')
 }
 
 if (Object.hasOwn(fruits, 'carrot')) {
-  console.log('carrot exists');
+  console.log('carrot exists')
 }
 
 if (!fruits.dragon) {
-  console.log('dragon does not exist');
+  console.log('❌ dragon does not exist')
 }
